e50332ac10993cea28a90987608bdf01b2ab3be1
Mostafa <your_email@example.com>
2026-04-24 20:11:15 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages , setup
+
+setup(
+    name = "medical_chatbot",
+    version = "0.1.0",
+    author = "Ahmed Mahmoud Mohamed",
+    author_email = "ahmedmahmoudmohamedd0@gmail.com",
+    packages = find_packages(),
+    install_requires = [] 
+)
